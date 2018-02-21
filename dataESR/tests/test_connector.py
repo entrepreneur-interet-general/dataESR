9d1657fb52e0e1c98309955bd0b8a2c88b22673d
Sami Moustachir <moustachir.sami@gmail.com>
2018-02-21 12:19:26 +0100

    adding test to connector

---
+++ dataESR/tests/test_connector.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+from mock import patch, Mock, MagicMock
+from connector import ConnectorDatabase
+
+class TestConnector(unittest.TestCase):
+    """Testing connector for different type of database"""
+
+    @patch("psycopg2.connect")
+    def test_connector_psql(self, psqlconnect):
+        path_dir = os.path.dirname(os.path.abspath(__file__))
+        filename = os.path.join(path_dir, "testconfig/testdatabase.ini")
+        connector = ConnectorDatabase(filename)
+        connector._connect_to_psql()
+        psqlconnect.assert_called_with(**connector.params.get_config(section='postgresql'))
