f6ad65bb4a6c6e68de66469156ec4b3f0e68e462
Sami Moustachir <moustachir.sami@gmail.com>
2018-02-21 18:29:19 +0100

    comment

--- dataESR/main.py
+++ dataESR/main.py
@@ -1,5 +1,10 @@
 """
 Test file for a first flow
+
+Current simple flow is :
+
+Connector >> Datasets >> Recipe >> Recipes 
+
 """
 
 import os
@@ -7,9 +12,12 @@ from connector import ConnectorDatabase
 from recipes import Datasets
 
 def function_to_apply(dataset):
+    """Take as an input a dataset, make relevant
+    changes and return a new dataset"""
     pass
 
 if __name__ == '__main__':
+
     path_dir = os.path.dirname(os.path.abspath(__file__))
     filename = os.path.join(path_dir, "config/example/database.ini")
     connector = ConnectorDatabase(filename)
