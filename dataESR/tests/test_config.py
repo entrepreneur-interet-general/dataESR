e79040b31b4f2425454edc17158b519464656e48
Sami Moustachir <moustachir.sami@gmail.com>
2018-02-21 10:50:29 +0100

    adding section to test

--- dataESR/tests/test_config.py
+++ dataESR/tests/test_config.py
@@ -1,14 +1,14 @@
 import unittest
-from config.config import ConfigDatabase
 import ConfigParser
 import os
+from config.config import ConfigDatabase
 
 class TestConfigDatabase(unittest.TestCase):
     def test_config_database(self):
         path_dir = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(path_dir, "testconfig/testdatabase.ini")
         self.assertEquals(
-            ConfigDatabase(filename).config_dbs,
+            ConfigDatabase(filename).get_config(),
             {
                 'postgresql': {
                     "host": "localhost",
