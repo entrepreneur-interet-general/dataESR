43374f4d9e9a03c0280fe5c9e0ae74625f944817
Sami Moustachir <moustachir.sami@gmail.com>
2018-06-04 11:40:11 +0200

    init airflow

---
+++ dataESR/airflow/dags/tagger.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'tutorial', default_args=default_args, schedule_interval=timedelta(1))
+
