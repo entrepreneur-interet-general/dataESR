665c4321ecab6d3fec678101c1cdb2ab90126f67
Sami Moustachir <moustachir.sami@gmail.com>
2018-06-06 15:47:23 +0200

    fix mongo_plugin git issue

---
+++ airflow/dags/mongo_plugin/__init__.py
@@ -0,0 +1,11 @@
+from airflow.plugins_manager import AirflowPlugin
+from mongo_plugin.hooks.mongo_hook import MongoHook
+
+class MongoPlugin(AirflowPlugin):
+    name = "MongoPlugin"
+    hooks = [MongoHook]
+    executors = []
+    macros = []
+    admin_views = []
+    flask_blueprints = []
+    menu_links = []
