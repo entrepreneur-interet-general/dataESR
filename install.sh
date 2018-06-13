39e2f0568f359c5ad524fbf07b5705fe9a54ff1b
Sami Moustachir <moustachir.sami@gmail.com>
2018-06-13 10:47:50 +0200

    update makefile/travis

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+
+set -e
+
 echo '-----------------------------------'
 echo '-----setting local environment-----'
 echo '-----------------------------------'
 
 
+echo 'exporting env. var'
 export DISK_LOCATION=/mnt/disk1
 export DATA_LOCATION=$DISK_LOCATION/data
 
@@ -18,4 +22,16 @@ export AIRFLOW_HOME=$DISK_LOCATION/dataESR/airflow
 export MONGO_VOLUME=$DATA_LOCATION/mongo
 export POSTGRESQL_VOLUME=$DATA_LOCATION/psql
 
-echo 'Finished exporting var.'
+echo 'Finished exporting var.\n'
+
+echo 'Checking environnement'
+
+for i in ${DISK_LOCATION} ${DATA_LOCATION} ${AIRFLOW_HOME} ${MONGO_VOLUME} ${POSTGRESQL_VOLUME}
+    do 
+        if [[ -z "${i}" ]]; then
+            echo '$i is not set'
+            exit 1
+        fi
+    done
+
+echo 'Environment checked: all variables are set.\n'
