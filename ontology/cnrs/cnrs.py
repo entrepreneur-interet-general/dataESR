5ac7467ab9e68457ebc1de5109950cd78d6f5c02
Sami Moustachir <moustachir.sami@gmail.com>
2018-07-19 18:34:29 +0200

    sparq

--- ontology/cnrs/cnrs.py
+++ ontology/cnrs/cnrs.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 from pymongo import MongoClient
-from pywikibot.data import api
+from pywikibot.data import api, sparql
+from pywikibot import pagegenerators as pg
 from pprint import pprint
 import pywikibot
 import tqdm
@@ -32,10 +33,33 @@ def match_cnrs_to_wikidata(collection, doc):
     collection.insert_one(doc)
 
 
+def sparkQL_query(site, query):
+    return pg.WikidataSPARQLPageGenerator(query, site=site)
+
+
+def get_info_from_id(repo, _id):
+    item = pywikibot.ItemPage(repo, _id).get()
+    if 'en' in item['labels'] and 'en' in item['descriptions']:
+        return {'_id': _id, 'label': item['labels']['en'],
+                'description': item['descriptions']['en']}
+    else:
+        return None
+
+
 if __name__ == '__main__':
     site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
-
+    query = """
+    SELECT ?item ?itemLabel 
+    WHERE 
+    {
+    ?item wdt:%s wd:%s.
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+    """
+    dependencies = {'endpoint': None, 'entity_url': None}
+    query_object = sparql.SparqlQuery(**dependencies)
+    science = re.compile(r'(field|stud|academic|discipline|science)(y\b|ies\b|ied\b|\b|s\b)', re.IGNORECASE)
     subject = re.compile(r'.+?(?= \(|:|-)')
     infos = re.compile(r'\((.+?)(-|:)(.+?)\)')
     sc = re.compile(r'(.+?)( - | : )(.+)')
@@ -45,9 +69,38 @@ if __name__ == '__main__':
     titres = [re.search(subject, x).group(0) for x in read_file('cnrs_annuaire')]
     print('Total titres: {}'.format(len(titres)))
     total = 0
+    set_titre = set()
     for l in read_file('cnrs_annuaire'):
         titre = re.search(subject, l).group(0)
         if 'Sc.' in titre:
             titre = re.search(sc, l).group(3)
-        coll.insert_one({'terme': titre})
-
+        set_titre.add(titre)
+    for i in tqdm.tqdm(set_titre):
+        search = get_items(site, i)['search']
+        pprint(search)
+        if len(search) > 0:
+            desc = search[0].get('description')
+            if desc is None:
+                continue
+            if science.search(desc):
+                total += 1
+                d = {
+                    'id': search[0]['id'],
+                    'description': search[0]['description'],
+                    'label': search[0]['label'],
+                    'titre': i
+                }
+                for prop in ['P527', 'P279', 'P361', 'P31']:
+                    query_f = query % (prop, d['id'])
+                    data = query_object.get_items(query_f,
+                                                  item_name='item',
+                                                  result_type=set)
+                    if len(data) == 0:
+                        continue
+                    arr = []
+                    for info_id in data:
+                        arr.append(get_info_from_id(repo, info_id))
+                    d.update({prop: arr})
+                    print("Found for prop {} and label {}: {} elements"
+                          .format(prop, d['label'], len(data)))
+                coll.insert_one(d)
