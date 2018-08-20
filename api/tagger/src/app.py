1a3c02e391794c7c39f1a8e323a9b92a2d61e969
Sami Moustachir <moustachir.sami@gmail.com>
2018-08-20 16:52:44 +0200

    update api

--- api/tagger/src/app.py
+++ api/tagger/src/app.py
@@ -1,15 +1,55 @@
-from flask import Flask
+from flask import Flask, json, request, abort
 from flask_restplus import Resource, Api
+import textacy
+import textacy.keyterms as tck
 
 app = Flask(__name__)
 api = Api(app)
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class TextacyFormatting(object):
+    def __init__(self, data, lang=None):
+        self.data = data
+        self.lang = self._detect_lang(data['text']) if not lang else lang
+        if 'method' in data:
+            self.method = data['method']
+        else:
+            self.method = 'sgrank'
 
+    def _detect_lang(self, text):
+            return textacy.text_utils.detect_language(text)
+
+    def _apply_keyterm_ranking(self, doc, params=None):
+        if self.method == 'sgrank':
+            keywords = textacy.keyterms.sgrank(doc, **params) \
+                if params else tck.sgrank(doc)
+        elif self.method == 'textrank':
+            keywords = textacy.keyterms.textrank(doc, **params) \
+                if params else tck.textrank(doc)
+        elif self.method == 'singlerank':
+            keywords = textacy.keyterms.singlerank(doc, **params) \
+                if params else tck.singlerank(doc)
+        return keywords
+
+    def get_keyterms(self, params=None):
+        doc = textacy.Doc(self.data['text'].decode('utf-8'), lang=self.lang)
+        keywords = self._apply_keyterm_ranking(doc, params)
+        return keywords
+
+
+@api.route('/keywords')
+class TextacyResponse(Resource):
+    def post(self):
+        data = request.json
+        if 'text' not in data:
+            abort(400, "No parameter text was founds.")
+        else:
+            tc = TextacyFormatting(data, lang=data.get('lang'))        
+            try:
+                keywords = tc.get_keyterms()
+                return json.dump({'keywords': keywords})
+            except Exception as e:
+                abort(400, e)        
 
 if __name__ == '__main__':
     app.run(debug=True)
--- api/tagger/tests/test_api.py
+++ api/tagger/tests/test_api.py
@@ -1,9 +1,59 @@
-import json
+from flask import json
 from src.app import app
+import unittest
 
 
-def test_hello_world():
-    test_client = app.test_client()
-    response = test_client.get('/hello')
-    assert response.status_code == 200
-    assert json.loads(response.data.decode('utf8')) == {'hello': 'world'}
+class TestTextacyResponse(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = app.test_client()
+
+    def test_doc_textacy(self):
+        params = {
+                'text': 'The next important step might be the derivation of \
+                        the Dirac equation. The Creutz model  [32] suggests \
+                        that we should consider incorporating into the logical \
+                        inference treatment, the additional knowledge that one \
+                        has objects hopping on a lattice instead of particles \
+                        moving in a space-time continuum. Recall that up to Section \
+                        2.4, the description of the measurement scenario, robustness \
+                        etc. is explicitly discrete. In Section  2.4, the continuum \
+                        limit was taken only because our aim was to derive the Pauli \
+                        equation, which is formulated in continuum space-time. Of \
+                        course, the description of the motion of the particle in \
+                        Section 2.6 is entirely within a continuum description but \
+                        there is no fundamental obstacle to replace this treatment \
+                        by a proper treatment of objects hopping on a lattice. \
+                        Therefore it seems plausible that the logical inference \
+                        approach can be extended to describe massless spin-1/2 \
+                        particles moving in continuum space-time by considering \
+                        the continuum limit of the corresponding lattice model. \
+                        An in-depth, general treatment of this problem is beyond \
+                        the scope of the present paper and we therefore leave this \
+                        interesting problem for future research.'
+                }
+        response = self.client.post('/keywords', data=params, content_type='application/json')
+        response_json = json.loads(response.data.decode('utf8'))
+
+        assert response_json['keywords'] == [(u'massless spin-1/2 particle',
+                                    0.15387941999481636),
+                                   (u'logical inference treatment',
+                                    0.11715332737705884),
+                                   (u'logical inference approach',
+                                    0.09136901016959438),
+                                   (u'continuum limit',
+                                    0.08033973111784386),
+                                   (u'continuum space',
+                                    0.06630463582851502),
+                                   (u'pauli equation',
+                                    0.03132777999751102),
+                                   (u'creutz model',
+                                    0.026581337388402043),
+                                   (u'fundamental obstacle',
+                                    0.02639697723912133),
+                                   (u'continuum description',
+                                    0.026300408365873586),
+                                   (u'proper treatment',
+                                    0.025135452800462643)]
+
